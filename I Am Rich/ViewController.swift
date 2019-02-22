fcf15e85bc007ea134911b0f750bccddb385be0f
Emanuel Covaci <covaciemanuel98@gmail.com>
2019-02-22 15:17:14 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Emanuel Covaci on 22/02/2019.
+//  Copyright © 2019 Emanuel Covaci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
